eb82daa1ee2c9687b5d12801003b367244ad2251
lazycece <lazycece@gmail.com>
2021-10-24 15:58:58 +0800

    add util

---
+++ rapidf-utils/src/main/java/com/lazycece/rapidf/utils/AsyncUtils.java
@@ -0,0 +1,46 @@
+/*
+ *    Copyright 2021 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.rapidf.utils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author lazycece
+ * @date 2018/4/6
+ */
+public class AsyncUtils {
+
+    private static AsyncUtils instance;
+
+    private ExecutorService executorService;
+
+    private AsyncUtils() {
+        executorService = Executors.newFixedThreadPool(100);
+    }
+
+    public static AsyncUtils getInstance() {
+        if (instance == null) {
+            instance = new AsyncUtils();
+        }
+        return instance;
+    }
+
+    public void execute(Runnable runnable) {
+        executorService.execute(runnable);
+    }
+}

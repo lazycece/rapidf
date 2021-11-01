8b77cc10c284385df2d87719f872afa62f5fb239
lazycece <lazycece@gmail.com>
2021-11-01 22:37:11 +0800

    init default

---
+++ rapidf-utils/src/main/java/com/lazycece/rapidf/utils/DefaultUtils.java
@@ -0,0 +1,30 @@
+/*
+ *    Copyright 2021 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.rapidf.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author lazycece
+ * @date 2021/10/31
+ */
+public class DefaultUtils {
+
+    public static String defaultValue(final String value, final String defaultValue) {
+        return StringUtils.isNotBlank(value) ? value : defaultValue;
+    }
+}

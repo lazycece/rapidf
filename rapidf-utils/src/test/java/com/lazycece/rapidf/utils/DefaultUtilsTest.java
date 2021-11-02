d1c7610821e01a921e108285b8e5201765a8a000
lazycece <lazycece@gmail.com>
2021-11-02 22:59:59 +0800

    validate ...

---
+++ rapidf-utils/src/test/java/com/lazycece/rapidf/utils/DefaultUtilsTest.java
@@ -0,0 +1,58 @@
+/*
+ *    Copyright 2021 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.rapidf.utils;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author lazycece
+ * @date 2021/11/1
+ */
+public class DefaultUtilsTest {
+
+    @Test
+    public void testDefaultValue() {
+        assertThat(DefaultUtils.defaultValue("value", "defaultValue"))
+                .isEqualTo("value");
+        assertThat(DefaultUtils.defaultValue("", "defaultValue"))
+                .isEqualTo("defaultValue");
+    }
+
+    @Test
+    public void testDefaultList() {
+        assertThat(DefaultUtils.defaultList(null)).isNotNull();
+        assertThat(DefaultUtils.defaultList(Lists.newArrayList())).isNotNull();
+    }
+
+    @Test
+    public void testDefaultMap() {
+        assertThat(DefaultUtils.defaultMap(null)).isNotNull();
+        assertThat(DefaultUtils.defaultMap(Maps.newHashMap())).isNotNull();
+    }
+
+    @Test
+    public void testDefaultValueIfNullObj() {
+        assertThat(DefaultUtils.defaultValueIfNullObj("value", String::toString,
+                "defaultValue")).isEqualTo("value");
+        assertThat(DefaultUtils.defaultValueIfNullObj(null, String::toString,
+                "defaultValue")).isEqualTo("defaultValue");
+    }
+}

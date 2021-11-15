f80d60c3b0f3f193e89e0e5ef24777eccf288829
lazycece <lazycece@gmail.com>
2021-11-16 00:08:15 +0800

    init rapidf module

---
+++ rapidf-example/src/test/java/com/lazycece/rapidf/rapidf/example/RapidfExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazycece.rapidf.rapidf.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidfExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

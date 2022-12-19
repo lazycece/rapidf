c2f73fbe78d442109c18a5c23b5098cc1b260357
lazycece <lazycece@gmail.com>
2022-12-20 00:23:01 +0800

    define basic domain info: entity, value object

---
+++ rapidf-domain/src/main/java/com/lazycece/rapidf/domain/entity/Entity.java
@@ -0,0 +1,71 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.rapidf.domain.entity;
+
+import java.util.Date;
+
+/**
+ * @author lazycece
+ * @date 2022/12/11
+ */
+public abstract class Entity<T> {
+
+    private String creator;
+    private String updater;
+    private Date createTime;
+    private Date updateTime;
+
+    /**
+     * Get the entity's business id
+     * <p>For domain, every entity should have its own business id. </p>
+     *
+     * @return business id
+     */
+    public abstract T getBizId();
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getUpdater() {
+        return updater;
+    }
+
+    public void setUpdater(String updater) {
+        this.updater = updater;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}

5b5f10fd2ee51a70ae2bcdd5252401ae46b2af0b
lazycece <lazycece@gmail.com>
2023-05-09 00:10:03 +0800

    init sample

---
+++ rapidf-samples/rapidf-auapi-sample/src/main/java/com/example/rapidf/auapi/sample/RapidfAuapiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.rapidf.auapi.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidfAuapiSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapidfAuapiSampleApplication.class, args);
+    }
+
+}

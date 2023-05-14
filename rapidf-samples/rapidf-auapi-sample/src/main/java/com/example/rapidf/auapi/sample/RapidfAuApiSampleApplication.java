5bbb9f0dd90ff664ea22df666f349fedf7547eca
lazycece <lazycece@gmail.com>
2023-05-15 01:53:58 +0800

    process auto config

--- rapidf-samples/rapidf-auapi-sample/src/main/java/com/example/rapidf/auapi/sample/RapidfAuApiSampleApplication.java
+++ rapidf-samples/rapidf-auapi-sample/src/main/java/com/example/rapidf/auapi/sample/RapidfAuApiSampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RapidfAuapiSampleApplication {
+public class RapidfAuApiSampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RapidfAuapiSampleApplication.class, args);
+        SpringApplication.run(RapidfAuApiSampleApplication.class, args);
     }
 
 }
---
+++ rapidf-samples/rapidf-auapi-sample/src/main/java/com/example/rapidf/auapi/sample/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.rapidf.auapi.sample.controller;
+
+import com.example.rapidf.auapi.sample.request.LoginRequest;
+import com.lazycece.au.api.token.SubjectContext;
+import com.lazycece.au.api.token.TokenHolder;
+import com.lazycece.rapidf.auapi.extension.UserSubject;
+import com.lazycece.rapidf.restful.response.RespData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author lazycece
+ */
+@RestController
+@RequestMapping("/u")
+@Slf4j
+public class UserController {
+
+    @Resource
+    private TokenHolder tokenHolder;
+
+    @PostMapping("/login")
+    public Object login(@RequestBody LoginRequest request, HttpServletResponse response) throws Exception {
+        log.info("username -> {}, password = {}", request.getUsername(), request.getPassword());
+        UserSubject subject = new UserSubject();
+        subject.setUsername(request.getUsername());
+        String token = tokenHolder.generateToken(subject);
+        response.addHeader(tokenHolder.getTokenHeader(), token);
+        return RespData.success();
+    }
+
+    @GetMapping("/info")
+    public Object getUserInfo() {
+        UserSubject subject = (UserSubject) SubjectContext.getContext();
+        return RespData.success(subject);
+    }
+}

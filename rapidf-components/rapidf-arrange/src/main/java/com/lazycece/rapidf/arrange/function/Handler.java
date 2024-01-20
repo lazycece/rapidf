71dca0b7ec8ab7582e4259bdbfadc484d2f5ec4a
lazycece <lazycece@gmail.com>
2024-01-20 21:03:38 +0800

    function

---
+++ rapidf-components/rapidf-arrange/src/main/java/com/lazycece/rapidf/arrange/function/Handler.java
@@ -0,0 +1,33 @@
+/*
+ *    Copyright 2024 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.rapidf.arrange.function;
+
+/**
+ * The handler functional interface .
+ *
+ * @author lazycece
+ * @date 2024/1/20
+ */
+public interface Handler<T> {
+
+    /**
+     * Handle something.
+     *
+     * @param t t
+     */
+    void handle(T t);
+}
